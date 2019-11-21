86fa8c7fb6c17a3d93421c1d0266f4d5a2ab11ef
Giridharan GM <giridharangm@outlook.com>
2019-11-21 17:39:33 +0530

    Remove old babel and rollup config

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
-
-export default {
-  input: "src/index.js",
-  output: { file: "lib/index.js", format: "cjs" },
-  plugins: [babel({ exclude: "node_modules/**" }), uglify()],
-  external: ["react", "prop-types"]
-};
